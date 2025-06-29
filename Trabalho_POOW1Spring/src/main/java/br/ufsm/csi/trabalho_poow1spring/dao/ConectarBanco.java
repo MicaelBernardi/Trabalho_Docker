b53e57225f8244f4c7290e58ae1d91efaa68ed24
MicaelBernardi <143213884+MicaelBernardi@users.noreply.github.com>
2025-06-29 13:49:27 -0300

    Update ConectarBanco.java

--- Trabalho_POOW1Spring/src/main/java/br/ufsm/csi/trabalho_poow1spring/dao/ConectarBanco.java
+++ Trabalho_POOW1Spring/src/main/java/br/ufsm/csi/trabalho_poow1spring/dao/ConectarBanco.java
@@ -1,1 +1,39 @@
 
+package br.ufsm.csi.trabalho_poow1spring.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectarBanco {
+
+public static Connection conectarBancoPostgres() throws ClassNotFoundException, SQLException {
+    Class.forName("org.postgresql.Driver");
+    System.out.println("Driver carregado");
+
+    String url = System.getenv("SPRING_DATASOURCE_URL");
+    String user = System.getenv("SPRING_DATASOURCE_USERNAME");
+    String senha = System.getenv("SPRING_DATASOURCE_PASSWORD");
+
+    System.out.println("URL: " + url);
+    System.out.println("User: " + user);
+
+    if (url == null) {
+        url = "jdbc:postgresql://localhost:5432/trabalhopoow1";  // fallback para desenvolvimento local
+    }
+    if (user == null) {
+        user = "postgres";
+    }
+    if (senha == null) {
+        senha = "1234";
+    }
+
+    Connection conn = DriverManager.getConnection(url, user, senha);
+    return conn;
+}
+
+    public Connection conectarBancoPostgresMySql(){
+        return null;
+    }
+}
+
